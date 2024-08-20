32348c9f1343b50e88b8e113ff3d3dbf64866e48
Harmain1233 <124540022+Harmain1233@users.noreply.github.com>
2024-08-20 18:40:08 -0400

    Update EmailSection.jsx

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -10,16 +10,16 @@ const ContactSection = () => {
     >
       <div className="flex flex-col items-center max-w-6xl mx-auto">
         <h5 className="text-3xl font-bold mb-4 text-center">
-          Let's Connect
+          Lets Connect
         </h5>
         <p className="text-gray-400 mb-8 max-w-md text-center text-lg">
-          I'm currently looking for new opportunities, and my inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!
+          Im currently looking for new opportunities, and my inbox is always open. Whether you have a question or just want to say hi, Ill try my best to get back to you!
         </p>
         <div className="flex justify-end gap-6 mt-4">
-          <Link href="https://github.com" target="_blank" rel="noopener noreferrer">
+          <Link href="https://github.com/Harmain1233" target="_blank" rel="noopener noreferrer">
             <FaGithub className="text-white text-4xl hover:text-gray-400 transition-colors" />
           </Link>
-          <Link href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+          <Link href="https://www.linkedin.com/in/harmain-munir-335511243/" target="_blank" rel="noopener noreferrer">
             <FaLinkedin className="text-white text-4xl hover:text-gray-400 transition-colors" />
           </Link>
         </div>
