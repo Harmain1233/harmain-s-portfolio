dc4e84a3b7ff8b1ad4ceb57997d329e8c0d6a6de
Harmain1233 <124540022+Harmain1233@users.noreply.github.com>
2024-08-30 01:21:30 -0400

    Update ProjectsSection.jsx

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -1,89 +1,90 @@
-import React from 'react';
+"use client";
+import React, { useState, useRef } from "react";
 import ProjectCard from "./ProjectCard";
+import { motion, useInView } from "framer-motion";
 
 const projectsData = [
     {
       id: 1,
-      title: "Reminder Requests Full Stack Project",
-      description: "Hover to see more!",
+      title: "Reminder Requests App",
+      description: "Full Stack Hackathon Project",
       image: "/images/img2.png", 
-      tag: ["All", "Web"],
       gitUrl: "https://github.com/bhaw16/Hack-Brooklyn-Hackathon-Project",
       previewUrl: "https://devpost.com/software/reminder-requests-app",
     },
     {
       id: 2,
-      title: "Career Buddy Project",
-      description: "Hover to see more",
-      image: "/images/img1.png", 
-      tag: ["All", "Web"],
+      title: "CUNYGPT Project",
+      description: "Full Stack CTP Hackathon Project",
+      image: "/images/imageCUNYGPT.png", 
       gitUrl: "https://github.com/Harmain1233/CareerBuddy",
       previewUrl: "https://harmain1233.github.io/CareerBuddy/",
     },
     {
       id: 3,
+      title: "Career Buddy",
+      description: "Human Computer Interaction Project",
+      image: "/images/img1.png", 
+      gitUrl: "https://github.com/Harmain1233/CareerBuddy",
+      previewUrl: "https://harmain1233.github.io/CareerBuddy/",
+    },
+    {
+      id: 4,
       title: "Schedule Assistant",
-      description: "Hover to see more",
+      description: "Personal Project",
       image: "/images/img3.png", 
-      tag: ["All", "Web"],
       gitUrl: "https://github.com/Harmain1233/Schedule-Assistant",
       previewUrl: "https://harmain1233.github.io/Schedule-Assistant/",
     },
     {
-      id: 4,
+      id: 5,
       title: "To Do List Application",
-      description: "Hover to see more",
+      description: "Human Computer Interaction Project",
       image: "/images/img4.png", 
-      tag: ["All", "Mobile"],
       gitUrl: "https://github.com/Harmain1233/To-Do-List",
       previewUrl: "https://harmain1233.github.io/To-Do-List/",
     }
 ];
 
 const ProjectsSection = () => {
-    return (
-        <section id="projects">
-          
-            <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8">My Projects</h2>
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
 
-            <div className="overflow-x-auto whitespace-nowrap mr-4">
-            <div className="animate-scroll">
-            <div className="flex space-x-4">
-                {projectsData.map((project) => (
-                    <ProjectCard
-                        key={project.id}
-                        title={project.title}
-                        description={project.description}
-                        imgUrl={project.image}
-                        tag={project.tag}
-                        gitUrl={project.gitUrl}
-                        previewUrl={project.previewUrl}
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
 
-
-                    />
-                ))}
-  {/* Duplicate the cards for seamless scrolling */}
-  {projectsData.map((project) => (
-                        <ProjectCard
-                            key={`${project.id}-duplicate`}
-                            title={project.title}
-                            description={project.description}
-                            imgUrl={project.image}
-                            tag={project.tag}
-                            gitUrl={project.gitUrl}
-                            previewUrl={project.previewUrl}
-                        />
-                    ))}
-
-
-
-            </div>
-            </div>
-            </div>
-            </section>
-
-
-    );
+  return (
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+         
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-2 gap-8 md:gap-12">
+        {projectsData.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default ProjectsSection;
