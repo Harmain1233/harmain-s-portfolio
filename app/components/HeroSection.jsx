4878c28044d564d71ffb9d383102ed740b304454
Harmain Munir <124540022+Harmain1233@users.noreply.github.com>
2025-03-28 16:30:32 -0400

    Update HeroSection.jsx

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -44,7 +44,7 @@ const HeroSection = () => {
             <Link href="/#contact" className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
                 Hire Me!</Link>
             <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
-            <a href="images/HarmainMunir_Resume_ConEdison.pdf" download>
+            <a href="images/HarmainMunir.Resume_Feb.15.2025 (3).pdf" download>
   <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
     Download Resume
   </span>
